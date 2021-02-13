2e9b35ebc938c68d654348d4bf5731a9c8fdd750
bharatsindha <sindhabharatsinh8@gmail.com>
2021-02-13 14:41:11 +0530

    Done the changes.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -18,8 +18,7 @@ class MY_Controller extends CI_Controller
         $this->data['plugins_dir'] = $this->config->item('plugins_dir');
         $this->data['avatar_dir'] = $this->config->item('avatar_dir');
 
-        $this->config->config["base_url"]="http://flooopadmin.local/";
-        $this->config->config["base_url"]="https://c2a2330d8590.ngrok.io/";
+        $this->config->config["base_url"]="http://floooplife.com/flooopadmin/";
         //echo base_url();exit;
         /* Any mobile device (phones or tablets) */
         if ($this->mobile_detect->isMobile())
