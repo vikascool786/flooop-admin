5c19d2584d248b06e2404b217a48dc04e0cdfd2c
bharatsindha <sindhabharatsinh8@gmail.com>
2021-02-20 17:06:16 +0530

    Done.

--- application/config/zoom.php
+++ application/config/zoom.php
@@ -2,7 +2,7 @@
 // Zoom App credential
 $config['ZOOM_CLIENT_ID'] = 'M5lp65SyTzCMWHvJmXE_sA';
 $config['ZOOM_CLIENT_SECRET'] = 'sQowadEg6gvH2owT2zmYtJ2INmUaAADc';
-$config['ZOOM_REDIRECT_URI'] = 'http://floooplife.com/flooopadmin/zoom/callback';
+$config['ZOOM_REDIRECT_URI'] = 'https://6eaf139dbd21.ngrok.io/flooopadmin/zoom/callback';
 $config['ZOOM_AUTHORISED_URL'] = "https://zoom.us/oauth/authorize?client_id=".$config['ZOOM_CLIENT_ID']."&response_type=code&redirect_uri=".$config['ZOOM_REDIRECT_URI'];
 
 
